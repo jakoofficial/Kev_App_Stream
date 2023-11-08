4c424d38be2df12c1b1fbbd7e557f8b52bdda6c7
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-11-08 09:40:24 +0100

    Made it so rules are stacked when the app goes into roll mode

--- Kev_App_Stream/ViewModels/AppMainScreenViewModel.cs
+++ Kev_App_Stream/ViewModels/AppMainScreenViewModel.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Kev_App_Stream.ViewModels
@@ -12,7 +14,7 @@ namespace Kev_App_Stream.ViewModels
     public class AppMainScreenViewModel : ViewModelBase
     {
         public List<string> PlayersList { get; set; } = AppManager.Instance.PlayerNameList;
-        public List<string> RuleList { get; set; } = AppManager.Instance.RuleList;
+        public List<string> RuleList { get; set; }
 
         ObservableCollection<string> history = new ObservableCollection<string>();
         public ObservableCollection<string> History { get => history; set => this.RaiseAndSetIfChanged(ref history, value); }
@@ -22,26 +24,45 @@ namespace Kev_App_Stream.ViewModels
         public string PlayerName { get => playerName; set => this.RaiseAndSetIfChanged(ref playerName, value); }
         public string Rule { get => rule; set => this.RaiseAndSetIfChanged(ref rule, value); }
 
-        public AppMainScreenViewModel() 
+        public AppMainScreenViewModel()
         {
             History.Clear();
             History = AppManager.Instance.History;
+
+            RuleList = RuleAmountCorrection();
+        }
+
+        public List<string> RuleAmountCorrection()
+        {
+            List<string> newRuleList = new List<string>();
+            List<string> tempRuleList = new List<string>();
+
+            foreach (var item in AppManager.Instance.RuleList)
+            {
+                int amount = AppManager.Instance.RuleList.Where(x => x.Equals(item)).Count();
+                tempRuleList.Add(item + " x" + amount);
+            }
+
+            for (int i = 0; i < tempRuleList.Count; i++)
+            {
+                string rule = tempRuleList[i];
+                if(!newRuleList.Contains(rule)) newRuleList.Add(rule);
+            }
+
+            return newRuleList;
         }
 
         Random rng = new Random();
         public void Roll()
         {
             int rName = rng.Next(PlayersList.Count);
-            int rRule = rng.Next(RuleList.Count);
-
-            //if (PlayerName == PlayersList[rName]) Roll();
+            int rRule = rng.Next(AppManager.Instance.RuleList.Count);
 
             PlayerName = PlayersList[rName];
-            Rule = RuleList[rRule];
+            Rule = AppManager.Instance.RuleList[rRule];
 
             string newHistoryAddition = String.Format("{2} | {0}\n{1}", PlayerName, Rule, DateTime.Now.ToString("HH:mm:ss"));
             History.Insert(0, newHistoryAddition);
-            //History = new ObservableCollection<string>(History.Reverse());
         }
 
         public void ClearHistory()
